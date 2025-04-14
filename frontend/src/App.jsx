7c1533b6b51c24745748b74bb35947caabd883f1
LOMBARDO Milo <milo.lombardo5@gmail.com>
2025-04-14 18:25:11 +0100

    update du app.jsx pour ouvrir manager recap dans une autre page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,36 +10,40 @@ function App() {
 
   return (
     <Router>
-      <div>
-        <div>
-          <a href="https://vite.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <h1>Vite + React</h1>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
-        <div>
-          <Link to="/manager-recap">
-            <button>Go to Manager Recap</button>
-          </Link>
-        </div>
-      </div>
-
       <Routes>
         <Route path="/manager-recap" element={<EmployeeAvailability />} />
+        <Route
+          path="/"
+          element={
+            <div>
+              <div>
+                <a href="https://vite.dev" target="_blank">
+                  <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+                <a href="https://react.dev" target="_blank">
+                  <img src={reactLogo} className="logo react" alt="React logo" />
+                </a>
+              </div>
+              <h1>Vite + React</h1>
+              <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>
+                  count is {count}
+                </button>
+                <p>
+                  Edit <code>src/App.jsx</code> and save to test HMR
+                </p>
+              </div>
+              <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+              </p>
+              <div>
+                <Link to="/manager-recap">
+                  <button>Go to Manager Recap</button>
+                </Link>
+              </div>
+            </div>
+          }
+        />
       </Routes>
     </Router>
   );
