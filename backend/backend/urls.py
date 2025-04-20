f054096694f9b1d7cc1858bfa89548ca6b9ea833
ch020 <chappersthepenguin@gmail.com>
2025-04-20 17:40:53 +0100

    Updated backend

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,7 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
-from rota.views import logout_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
